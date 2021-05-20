99d746678f9c269bf37ae08dd65e67cc80758fb1
MauricioGonCas <mauriciogonzalezcas@gmail.com>
2021-05-20 17:09:36 -0500

    primer

---
+++ src/main/java/com/dwa/practica/PracticaDwaApplication.java
@@ -0,0 +1,13 @@
+package com.dwa.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwa/practica/PracticaDwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwa.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
